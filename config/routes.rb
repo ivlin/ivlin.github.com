abb4340c71cefa30126426ec9793932506f138cb
minivan <ivlin112@gmail.com>
2017-11-06 12:02:28 -0500

    rails to pages

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  root "static_pages#home"
-  get 'home', to: 'static_pages#home'
-  get 'education', to: 'static_pages#education'
-  get 'experience', to: 'static_pages#experience'
-  get 'misc', to: 'static_pages#misc'
-  get 'resume', to: 'static_pages#resume'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
